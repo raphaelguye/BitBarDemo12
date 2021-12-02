82956a5353f891cce960f778195d70b406ca8a98
Raphael Guye <raphael.guye@gmail.com>
2021-12-02 16:10:19 +0100

    Initial Commit

---
+++ BitBarDemo12/ViewController.swift
@@ -0,0 +1,17 @@
+//
+// Created by Raphael Guye on 02.12.21
+// https://github.com/raphaelguye
+// 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
