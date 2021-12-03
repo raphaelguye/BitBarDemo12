dbaeb4ab0a3fdbd3e8148dce9fc81f240190f8e4
Raphael Guye <raphael.guye@gmail.com>
2021-12-03 15:13:22 +0100

    implement tests, fix xcodeproj for testing, adding .sauce directory for saucelabs config

--- BitBarDemo12Tests/BitBarDemo12Tests.swift
+++ BitBarDemo12Tests/BitBarDemo12Tests.swift
@@ -16,16 +16,11 @@ class BitBarDemo12Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testExample1() throws {
+      XCTAssertTrue(true)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testExample2() throws {
+      XCTAssertFalse(false)
     }
-
 }
